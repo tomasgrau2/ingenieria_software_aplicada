2007462579bf6f443497b9e5b01186e9d6448280
tomasgrau2 <t.grau@alumno.um.edu.ar>
2025-07-22 17:44:10 -0300

    Ionic

--- src/main/java/com/example/product/config/SecurityConfiguration.java
+++ src/main/java/com/example/product/config/SecurityConfiguration.java
@@ -76,6 +76,7 @@ public class SecurityConfiguration {
                     .requestMatchers(mvc.pattern("/api/activate")).permitAll()
                     .requestMatchers(mvc.pattern("/api/account/reset-password/init")).permitAll()
                     .requestMatchers(mvc.pattern("/api/account/reset-password/finish")).permitAll()
+                    .requestMatchers(mvc.pattern("/api/public/**")).permitAll()
                     .requestMatchers(mvc.pattern("/api/admin/**")).hasAuthority(AuthoritiesConstants.ADMIN)
                     .requestMatchers(mvc.pattern("/api/**")).authenticated()
                     .requestMatchers(mvc.pattern("/v3/api-docs/**")).hasAuthority(AuthoritiesConstants.ADMIN)
